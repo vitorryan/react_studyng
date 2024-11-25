2e9a6d5c1f42c4324a6add9cc75cff99f22e24a7
Vitor Ryan da Rosa <vitorryanrosaaa@hotmail.com>
2024-11-25 20:31:55 -0300

    Criando a pasta onde aprendemos a iniciliazar o react

---
+++ 1_INTRO/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <p>Inicio do Curso</p>
+    </div>
+  );
+}
+
+export default App;
